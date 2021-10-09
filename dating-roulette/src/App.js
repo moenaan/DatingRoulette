b2b9981b3e310bf93fdb4c1ecd151ad8eb0816f1
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-10-09 11:03:58 -0500

    added background and jumbotron to login

--- dating-roulette/src/App.js
+++ dating-roulette/src/App.js
@@ -4,12 +4,18 @@ import React from 'react';
  import Setup from './components/Setup'
 // import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 // import { FacebookLoginButton } from 'react-social-login-buttons';
+import { Jumbotron } from 'reactstrap';
 
-
-
-const App = () =>  <Form/>; 
-<Setup/>
-
+function App() {
+    return (
+        <div className ="App">
+            <Jumbotron>
+                <Form/>; 
+                <Setup/>
+            </Jumbotron>
+        </div>
+    )
+}
 
 
 export default App;
